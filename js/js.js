e1fa4fc09e301876b00b3919085e72b9a53fde78
SAIDYYOUNES <112939817+SAIDYYOUNES@users.noreply.github.com>
2022-10-31 20:08:26 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -16,3 +16,92 @@ window.onscroll = () => {
 	menu.classList.remove('bx-x');
 	navbar.classList.remove('open');
 };
+var P = 0; 
+function add(name,price) {
+    var firstDivContent = document.getElementById(name);
+    var outDivContent = document.getElementById(price);
+    var product = document.getElementById('e');
+    var total = document.getElementById('htotal');
+
+    let R = document.createElement("tr");
+    let A = document.createElement("td");
+    let B = document.createElement("td");
+   
+    
+    product.append(R);
+    A.innerHTML = firstDivContent.innerHTML;
+    B.innerHTML = outDivContent.innerHTML + "$";
+    
+    
+    var Prix =Number(outDivContent.innerHTML);
+    
+    P = P + Prix;
+    
+    R.append(A,B);
+    total.innerHTML=P+"$";
+    }
+    
+    function filter(item){
+        var sushi1=document.getElementById('c3');
+        var sushi2=document.getElementById('c4');
+        var sushi3=document.getElementById('c5');
+        var sushi4=document.getElementById('c6');
+        var soup1=document.getElementById('c1');
+        var soup2=document.getElementById('c2');
+        var salade1=document.getElementById('c7');
+        var salade2=document.getElementById('c8');
+        if(item=='all'){
+            sushi1.style.display = "block"
+            sushi2.style.display = "block"
+            sushi3.style.display = "block"
+            sushi4.style.display = "block"
+            soup1.style.display = "block"
+            soup2.style.display = "block"
+            salade1.style.display = "block"
+            salade2.style.display = "block"
+            
+    
+            
+        }
+        if(item=='sushi'){
+            sushi1.style.display = "block"
+            sushi2.style.display = "block"
+            sushi3.style.display = "block"
+            sushi4.style.display = "block"
+            soup1.style.display = "none"
+            soup2.style.display = "none"
+            salade1.style.display = "none"
+            salade2.style.display = "none"
+    
+        }
+        if(item=='salade'){
+            sushi1.style.display = "none"
+            sushi2.style.display = "none"
+            sushi3.style.display = "none"
+            sushi4.style.display = "none"
+            soup1.style.display = "none"
+            soup2.style.display = "none"
+            salade1.style.display = "block"
+            salade2.style.display = "block"
+    
+        }
+        if(item=='soup'){
+            sushi1.style.display = "none"
+            sushi2.style.display = "none"
+            sushi3.style.display = "none"
+            sushi4.style.display = "none"
+            soup1.style.display = "block"
+            soup2.style.display = "block"
+            salade1.style.display = "none"
+            salade2.style.display = "none"
+    
+        }
+    }
+    var a=document.getElementById('menu').value;
+    console.log(a)
+    function select(a){
+    
+   
+
+}
+   
